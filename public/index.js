63b1a170126e49a4da3e71c79d9a37f45aff0d7a
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2018-06-25 23:14:10 -0700

    frontend fixes

--- public/index.js
+++ public/index.js
@@ -1,19 +1,77 @@
-
 function triggerGetWeather(callback) {
   const url = "/weather"
   $.getJSON(url, callback)
 }
 
 
-
-function renderData(data) {
-  console.log(data)
+function splitData(data) {
+  let aprilData = data.slice(0, 30)
+  let mayData = data.slice(30, 61)
+  renderData(aprilData, "april-contain")
+  renderData(mayData, "may-contain")
 }
 
+function renderData(monthData, monthDiv) {
+
+  let totalHeat = 0
+  let totalAir = 0
+
+  monthData.forEach(day => {
+    if (day.air > 0) {
+      totalAir += 1
+    }
+    if (day.heat > 0) {
+      totalHeat += 1
+    }
+  })
+
+
+  let rows = monthData.map(day => {
+    return (
+  `    <div>
+        <div>${day.date}</div>
+        <div>${day.air}</div>
+        <div>${day.heat}</div>
+      </div>`
+    )
+  })
+
+  let chartHtml = `
+    <div>
+      <div>
+        <div>Summary</div>
+        <div>Total days where heat kicked on at least once: ${totalHeat}</div>
+        <div>Total days where AC kicked on at least once: ${totalAir}</div>
+        <div>Date</div>
+        <div>Hours of AC</div>
+        <div>Hours of Heat</div>
+      </div>
+
+      ${rows.join("")}
+    </div>
+  `
+
+let renderDiv = document.getElementById(monthDiv)
+renderDiv.innerHTML = chartHtml
+
+}
 
 function handleApp() {
-  triggerGetWeather(renderData)
+  triggerGetWeather(splitData)
 }
 
-//called on page load
+function openMonth(event, month) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(month).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
 $(handleApp)
--- server.js
+++ server.js
@@ -1,58 +1,59 @@
 const express = require("express");
 const app = express();
 const axios = require("axios")
-
 app.use(express.static("public"));
 
-let results = []
-
-let count = 0
 
 app.get("/weather", (req, res) => {
-  let weatherLoop = new Promise(function(resolve, reject) {
-    const secondsInDay = 86400
-    for (let i = 1522566000; i <= (secondsInDay * 2) + 1522566000; i += secondsInDay) {
-      getWeatherFromAPI(i)
-    }
-      resolve(results)
+  let timestampArr = createTs(1522566000, 60);
+
+  Promise.all(timestampArr.map(ts => {
+    return axios({
+      method: "get",
+      //if this code were being pushed to GitHub, put URL in a process.env for key security
+      url: `https://api.darksky.net/forecast/f648f417141dc92f572be83e29756ce1/45.5898,-122.5951,${ts}`,
+    }).then(data => data.data)
+  }))
+  .then(data => {
+    let responseData = []
+    data.forEach(day => {
+      let dayObject = manipulateData(day)
+      responseData.push(dayObject)
+    })
+    res.send(responseData);
   })
   .catch(err => {
     console.log(err)
-  })
-    weatherLoop.then(results => res.send(results))
-
+  });
 });
 
 
-function getWeatherFromAPI(i) {
-  return axios({
-    method: "get",
-    url: `https://api.darksky.net/forecast/dd37a44ba2f13043e928db948a4162e6/45.5898,-122.5951,${i}`,
-  })
-  .then(data => {
-    manipulateData(data)
-  }).catch(err => {
-    console.log(err)
-  })
+function createTs(startTs, days) {
+  let arr = [];
+  let secondsInDay = 86400;
+  for (let i = startTs; i <= (secondsInDay * days) + startTs; i += secondsInDay) {
+    arr.push(i);
+  }
+
+  return arr;
 }
 
 function manipulateData(data) {
+  let date = new Date(data.currently.time * 1000)
+  let dateString = date.toString().slice(0, 10)
   let resultObject = {
-    date: data.data.currently.time,
+    date: dateString,
     air: 0,
     heat: 0
   }
-  data.data.hourly.data.forEach(hour => {
+  data.hourly.data.forEach(hour => {
     if (hour.temperature > 75) {
       resultObject.air +=1
     } else if (hour.temperature < 62) {
       resultObject.heat +=1
     }
   })
-  console.log(resultObject)
-  results.push(resultObject)
-  count++
-  console.log(results, count)
+  return resultObject
 }
 
 
